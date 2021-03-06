ac63fd9c444681690848cffe0e366ed1218c20c7
Leonardo de Lima Oliveira <l3onardo.oliv3ira@gmail.com>
2021-03-06 11:26:21 -0400

    improvment

--- src/pje/br/jus/cnj/pje/office/gui/certlist/PjeCertificateListAcessor.java
+++ src/pje/br/jus/cnj/pje/office/gui/certlist/PjeCertificateListAcessor.java
@@ -1,20 +1,16 @@
 package br.jus.cnj.pje.office.gui.certlist;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.function.Predicate;
 
 import com.github.signer4j.ICertificate;
-import com.github.signer4j.ICertificateListUI.ICertificateEntry;
 import com.github.signer4j.ICertificates;
 import com.github.signer4j.IKeyStoreAccess;
-import com.github.signer4j.gui.CertificateListUI;
-import com.github.signer4j.imp.AbstractCertificateChooser;
-import com.github.signer4j.imp.Choice;
+import com.github.signer4j.imp.DefaultChooser;
 import com.github.signer4j.imp.IChoice;
 import com.github.signer4j.imp.exception.Signer4JException;
 
-public class PjeCertificateListAcessor extends AbstractCertificateChooser {
+public class PjeCertificateListAcessor extends DefaultChooser {
   
   public static final Predicate<ICertificate> SUPPORTED_CERTIFICATE =  c -> c.getKeyUsage().isDigitalSignature() && (c.hasCertificatePF() || c.hasCertificatePJ());
 
@@ -31,11 +27,6 @@ public class PjeCertificateListAcessor extends AbstractCertificateChooser {
   protected IChoice doChoose(List<CertificateEntry> options) throws Signer4JException {
     if (options.size() == 1)
       return toChoice(options.get(0));
-    @SuppressWarnings({ "unchecked", "rawtypes" })
-    Optional<ICertificateEntry> ce = CertificateListUI.display((List)options);
-    if (!ce.isPresent()) {
-      return Choice.CANCEL;
-    }
-    return toChoice((CertificateEntry)ce.get());
+    return super.doChoose(options);
   }
 }
