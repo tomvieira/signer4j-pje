ebbd2a5a24ff96e0829ba35e33b8f6352ee46f8a
leoolive <leoolive@trf3.jus.br>
2022-01-14 14:20:55 -0400

    fix package path

--- src/pje/br/jus/cnj/pje/office/core/IPjeConfigPersister.java
+++ src/pje/br/jus/cnj/pje/office/core/IPjeConfigPersister.java
@@ -2,8 +2,8 @@ package br.jus.cnj.pje.office.core;
 
 import java.util.Optional;
 
-import com.github.signe4j.imp.function.Performable;
 import com.github.signer4j.IConfigPersister;
+import com.github.signer4j.imp.function.Performable;
 
 import br.jus.cnj.pje.office.signer4j.IPjeAuthStrategy;
 
