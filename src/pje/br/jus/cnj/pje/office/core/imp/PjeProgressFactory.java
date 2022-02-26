7709d00492f675bd84e46cf820dc886c4e22761c
Leonardo de Lima Oliveira <l3onardo-oliv3ira@gmail.com>
2022-02-26 12:30:29 -0400

    Uniforming icon

--- src/pje/br/jus/cnj/pje/office/core/imp/PjeProgressFactory.java
+++ src/pje/br/jus/cnj/pje/office/core/imp/PjeProgressFactory.java
@@ -1,6 +1,5 @@
 package br.jus.cnj.pje.office.core.imp;
 
-import com.github.signer4j.progress.imp.Images;
 import com.github.signer4j.progress.imp.ProgressFactory;
 
 public class PjeProgressFactory extends ProgressFactory{
@@ -8,6 +7,6 @@ public class PjeProgressFactory extends ProgressFactory{
   public static final PjeProgressFactory DEFAULT = new PjeProgressFactory();
 
   private PjeProgressFactory() {
-    super(PjeConfig.getIcon(), Images.LOG.asIcon());
+    super(PjeConfig.getIcon());
   }
 }
