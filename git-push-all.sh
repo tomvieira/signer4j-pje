5dcf4c8a4752a8a794147b4f07a2259b9ea69024
LEOOLIVE <LEOOLIVE@FDRD-18251.trf3.jus.br>
2022-07-01 15:51:56 -0400

    fix script

--- git-push-all.sh
+++ git-push-all.sh
@@ -1,26 +1,26 @@
 cd ../utils4j
 git push origin
 
-cd ../filehandler4j.git
+cd ../filehandler4j
 git push origin
 
-cd ../pdfhandler4j.git
+cd ../pdfhandler4j
 git push origin
 
-cd ../videohandler4j.git
+cd ../videohandler4j
 git push origin
 
-cd ../progress4j.git
+cd ../progress4j
 git push origin
 
-cd ../taskresolver4j.git
+cd ../taskresolver4j
 git push origin
 
-cd ../signer4j.git
+cd ../signer4j
 git push origin
 
-cd ../signer4j-pje.git
+cd ../signer4j-pje
 git push origin
 
-cd ../cutplayer4jfx.git
+cd ../cutplayer4jfx
 git push origin
